46e66a36ff87c2b8a6f08b5f1e32f413e9011dcb
Lynda Foster <lfost42@wgu.edu>
2022-11-17 17:45:07 -0800

    Add docstring

--- week01/W1D4/Part1/pydantic/app.py
+++ week01/W1D4/Part1/pydantic/app.py
@@ -2,12 +2,12 @@
 An exercise in pydantic and validators
 """
 from pydantic import BaseModel, validator
-from datetime import date
+import datetime
 
 class Student(BaseModel):
     student_name: str
     student_id: int
-    date_of_birth: date
+    date_of_birth: datetime.date
     #student must be between 18 and 50 years old
     address: str
     state: str
@@ -27,14 +27,15 @@ class Student(BaseModel):
         """
         Validates student is between 18 and 50 years old. 
         """
-        if value.year > 2004 or value.year < 1972:
+        age = datetime.date.today() - value
+        if age < datetime.timedelta(days=6570) or age > datetime.timedelta(days=18250):
             raise ValueError('The student must be between 18 and 50 years old')
         return value
     
 student1 = Student(
     student_name = 'John Smith',
     student_id = 1111,
-    date_of_birth = '1996-12-15',
+    date_of_birth = '2009-12-15',
     address = '34 Main St',
     state = 'CA',
     zip_code = '09823'

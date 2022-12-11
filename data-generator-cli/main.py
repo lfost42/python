bbbae534faed11cdf56bd693e278e8692bbf2332
Lynda Foster <lfost42@wgu.edu>
2022-12-11 09:39:24 -0800

    Expanded config file

--- data-generator-cli/main.py
+++ data-generator-cli/main.py
@@ -2,13 +2,17 @@
 Main application module. 
 """
 import config
-from producer_applicant import create_admin_user, create_applicant, get_applicants
+from producer_applicant import create_applicant, get_applicants
+from producer_user import create_admin_login
 
 
 def main():
-    create_admin_user()
-    create_applicant(config.num_applicants)
-    get_applicants()
+    if config.create_admin == True:
+        create_admin_login()
+    if config.create_applicant == True:
+        create_applicant()
+    if config.get_applicants == True:
+        get_applicants()
 
 
 if __name__ == '__main__':
